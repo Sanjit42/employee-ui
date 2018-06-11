4662b5d465e565753bafd4460dc764edcc697dc0
Sanjit42 <sanjitprad@gmail.com>
2018-06-11 20:02:27 +0530

    Save only updated values

--- src/reducers/skillsAndAbilities.spec.js
+++ src/reducers/skillsAndAbilities.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/namespace */
 import React from 'react';
 import {expect} from 'chai';
 
@@ -68,5 +67,56 @@ describe('SkillsAndAbilities Reducer', () => {
     let wrapper = skillsAndAbilities(initialState, action);
     expect(wrapper.length).to.deep.equal(2);
     expect(wrapper).to.deep.equal(expected);
+  });
+
+
+  it('should update only new changes fields', () => {
+    let initialState = [
+      {
+        employeeId: 1,
+        technical: {AWS: 2, Java: 3},
+        consulting: {communication: 4, planning: 3, questioning: 2},
+        domain: {government: 4, education: 2},
+        testing: {capybara: 2, cucumber: 2}
+      },
+      {
+        employeeId: 2,
+        technical: {AWS: 2, Java: 3},
+        consulting: {communication: 4, planning: 2, questioning: 1},
+        domain: {government: 1, education: 2},
+        testing: {capybara: 2, cucumber: 0}
+      }];
+
+    const action = {
+      type: types.UPDATE_SKILLS_AND_ABILITIES_SUCCESS,
+      skillsAndAbilities: {
+        employeeId: 2,
+        technical: {Java: 4},
+        domain: {government: 3},
+        testing: {capybara: 1, cucumber: 4},
+        consulting: { planning: 3, questioning: 2}
+      }
+    };
+
+    let expected = [
+      {
+        employeeId: 1,
+        technical: {AWS: 2, Java: 3},
+        consulting: {communication: 4, planning: 3, questioning: 2},
+        domain: {government: 4, education: 2},
+        testing: {capybara: 2, cucumber: 2}
+      },
+      {
+        employeeId: 2,
+        technical: {AWS: 2, Java: 4},
+        consulting: {communication: 4, planning: 3, questioning: 2},
+        domain: {government: 3, education: 2},
+        testing: {capybara: 1, cucumber: 4}
+      }
+    ];
+
+    let wrapper = skillsAndAbilities(initialState, action);
+    expect(wrapper).to.deep.equal(expected);
+
   });
 });
--- src/reducers/skillsAndAbilitiesReducer.js
+++ src/reducers/skillsAndAbilitiesReducer.js
@@ -4,12 +4,28 @@ import _ from 'lodash';
 import * as types from '../constants/constant';
 import initialState from './initialState';
 
+function updateSkills(state, action) {
+  let toChange = state.filter(each => each.employeeId === action.skillsAndAbilities.employeeId);
+  let keys = Object.keys(action.skillsAndAbilities);
+  let subsets = _.pull(keys, "employeeId");
+  let updatedSkills = toChange[0];
+
+  subsets.map(each => {
+    let subset = {};
+    let updateSubset = Object.assign({}, toChange[0][each], action.skillsAndAbilities[each]);
+    subset[each] = updateSubset;
+    updatedSkills = Object.assign({}, updatedSkills, subset);
+  });
+
+  return updatedSkills;
+}
+
 export default function ratingReducer(state = initialState.skillsAndAbilities, action) {
   switch (action.type) {
     case types.UPDATE_SKILLS_AND_ABILITIES_SUCCESS:
       return [
         ...state.filter(each => each.employeeId !== action.skillsAndAbilities.employeeId),
-        Object.assign({}, action.skillsAndAbilities)
+        Object.assign({}, updateSkills(state, action))
       ];
     case types.LOAD_SKILLS_AND_ABILITIES_SUCCESS:
       return action.skillsAndAbilities;
