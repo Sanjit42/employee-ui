bd31eb8bdaeed2dd88a5dcc0331485f9bc3d32e2
Sanjit42 <sanjitprad@gmail.com>
2018-06-11 14:40:13 +0530

    Update redux store after uploaded image

--- src/reducers/avatarReducer.js
+++ src/reducers/avatarReducer.js
@@ -5,7 +5,10 @@ import initialState from './initialState';
 export default function avatarReducer(state = initialState.avatar, action) {
   switch (action.type) {
     case types.UPDATE_AVATAR_SUCCESS:
-      return action.avatar;
+      return [
+        ...state.filter(each => each.employeeId !== action.avatar.employeeId),
+        Object.assign({}, action.avatar)
+      ];
     case types.LOAD_AVATAR_SUCCESS:
       return action.avatar;
     default:
