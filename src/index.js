29c1602873d88be23af395c3b367b5f52f766f0a
Sanjit42 <sanjitprad@gmail.com>
2019-11-23 16:03:01 +0530

    Delete unuse route and app files

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import {BrowserRouter} from 'react-router-dom';
 
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
-import Routes from './routes';
 
 import {loadHomeOffices} from "./actions/homeOfficesActions";
 import {loadEmployees} from "./actions/employeeActions";
 import {loadGender} from "./actions/genderActions";
 import {loadAvatar} from "./actions/avatarActions";
 import {loadSkillsAndAbilities} from "./actions/skillsAndAbilitiesActions";
-// eslint-disable-next-line import/namespace
-import AboutApp from './components/aboutApp/AboutPage';
-import App from './App';
+import App from "./App";
 
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -28,15 +25,6 @@ store.dispatch(loadEmployees());
 store.dispatch(loadGender());
 store.dispatch(loadAvatar());
 store.dispatch(loadSkillsAndAbilities());
-
-// render(
-//   <Provider store={store}>
-//     < Router history={browserHistory} >
-//       <Route path="/" component={AboutApp}/>
-//     </Router>
-//   </Provider>,
-//   document.getElementById('app')
-// );
 
 render(
   <Provider store={store}>
